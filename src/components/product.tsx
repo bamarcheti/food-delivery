42eff53c5e62b8a393b2dc878b621ba89aa890d5
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2024-02-15 11:25:31 -0400

    feat: Adicionado preço ao cart do produto

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,3 +1,4 @@
+import { formatCurrency } from "@/utils/functions/format-currency";
 import { forwardRef } from "react";
 import {
   Image,
@@ -13,6 +14,7 @@ type ProductDataProps = {
   description: string;
   thumbnail: ImageProps;
   quantity?: number;
+  price: number;
 };
 
 type ProductProps = TouchableOpacityProps & {
@@ -27,7 +29,10 @@ export const Product = forwardRef<TouchableOpacity, ProductProps>(
         className="w-full flex-row items-center pb-4"
         {...rest}
       >
-        <Image source={data.thumbnail} className="w-20 h-20 rounded-md" />
+        <Image
+          source={data.thumbnail}
+          className="w-[110px] h-[110px] rounded-md"
+        />
 
         <View className="flex-1 ml-3">
           <View className="flex-row items-center">
@@ -44,6 +49,10 @@ export const Product = forwardRef<TouchableOpacity, ProductProps>(
 
           <Text className="text-secondary text-xs leading-5 mt-0.5">
             {data.description}
+          </Text>
+
+          <Text className="text-lime300 text-xs leading-5 mt-0.5">
+            {formatCurrency(data.price)}
           </Text>
         </View>
       </TouchableOpacity>
